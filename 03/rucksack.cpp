422e3aea976759dda82ccd2a493e7750c15e9a13
Mikko Malkavaara <mikko.malkkis@gmail.com>
2022-12-07 22:30:45 +0200

    Now there's a solution to the third exercise and jesus wept it's ugly - might want to rework it (T⌓T)

--- 03/rucksack.cpp
+++ 03/rucksack.cpp
@@ -2,46 +2,88 @@
 #include <string>
 #include <map>
 #include <algorithm>
+#include <array>
+#include <sstream>
+#include <fstream>
+#include <vector>
 
+#define COMPARTMENT false
 
 using namespace std;
 
-
-const string rucksack1 = "vJrwpWtwJgWrhcsFMMfFFhFp";
-const string rucksack2 = "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL";
-const string rucksack3 = "PmmdzqPrVvPwwTWBwg";
-const string rucksack4 = "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn";
-const string rucksack5 = "ttgJtRGJQctTZtZT";
-const string rucksack6 = "CrZsJsPPZsGzwwsLwLmpwMDw";
+const string rucksack = "vJrwpWtwJgWrhcsFMMfFFhFp\n"
+                        "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n"
+                        "PmmdzqPrVvPwwTWBwg\n"
+                        "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n"
+                        "ttgJtRGJQctTZtZT\n"
+                        "CrZsJsPPZsGzwwsLwLmpwMDw\n";
 const string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 
 map<char, int> constructPriorityList();
-map<char, int> findCommon(const string& input);
 
-int main(){
+vector<char> findCommon(const string &input1, const string &input2);
 
-    map<char, int> priority = constructPriorityList();
+vector<char> findCommonFromThree(const string &rucksack1, const string &rucksack2, const string &rucksack3);
 
-    cout << rucksack1.substr(0, rucksack1.size()/2) << endl;
-    cout << rucksack1.substr(rucksack1.size()/2, rucksack1.size()) << endl;
-    cout << rucksack1.find('v') << endl;
+int main() {
 
-    auto commonsMap = findCommon(rucksack1);
-    auto tmpMap = findCommon(rucksack2);
-    commonsMap.insert(tmpMap.begin(), tmpMap.end());
-    for(const auto &p : commonsMap){
-        cout << priority[p.first] << endl;
-        cout << p.first << " : " << p.second << " - " << priority[p.first] << endl;
+    stringstream ss;
+    string line;
+    auto is = ifstream("./rucksack.txt");
+
+    if (is) {
+        ss << is.rdbuf();
+        is.close();
+    } else {
+        cout << "file rucksack.txt not found in the same directory" << endl;
+        cout << "falling back to example data from the problem definition" << endl;
+        ss << rucksack;
     }
+    map<char, int> priority = constructPriorityList();
+    int prioSum = 0;
+
+    if (COMPARTMENT) {
+        vector<char> compartmentCommon;
+        while (getline(ss, line)) {
+            int half = static_cast<int>(line.size()) / 2;
+            auto tmp = findCommon(line.substr(0, half), line.substr(half, line.size()));
+            compartmentCommon.insert(compartmentCommon.end(), tmp.begin(), tmp.end());
+        }
+
+
+        for (const auto &c: compartmentCommon) {
+            cout << "found: " << c << ", priority:" << priority[c] << endl;
+            prioSum += priority[c];
+        }
+    } else {
+        int linecount = 0;
+        vector<char> rucksackCommon;
+        array<string, 3> rucksacks{"", "", ""};
+        while (getline(ss, line)) {
+            rucksacks[(linecount % 3)] = line;
+            linecount++;
+            if (linecount % 3 == 0) {
+                auto tmp = findCommonFromThree(rucksacks[0], rucksacks[1], rucksacks[2]);
+                rucksackCommon.insert(rucksackCommon.end(), tmp.begin(), tmp.end());
+            }
+
+        }
+        cout << linecount << " lines read" << endl;
+        for(const auto &item: rucksackCommon){
+            cout << item << endl;
+            prioSum += priority[item];
+        }
+    }
+    cout << "sum of all priorities: " << prioSum << endl;
     return EXIT_SUCCESS;
 }
 
 //associates every character from a-Z with value from 1 to 52
-map<char, int> constructPriorityList(){
+map<char, int> constructPriorityList() {
     map<char, int> resultMap;
     int val = 1;
-    for(char c: alphabet){
+    for (char c: alphabet) {
         resultMap.insert({c, val});
         val++;
     }
@@ -49,27 +91,37 @@ map<char, int> constructPriorityList(){
 }
 
 //takes one line/'rucksack' at the time
-map<char, int> findCommon(const string& input){
-    map<char, int> resultMap;
-    int half = static_cast<int>(input.size()) / 2;
-    string compartment1 = input.substr(0, half);
-    string compartment2 = input.substr(half, input.size());
+vector<char> findCommon(const string &input1, const string &input2) {
+    vector<char> result;
 
-    cout << compartment1 << " + " << compartment2 << endl;
-    auto f= compartment1.find('e');
-    cout << (f != std::string::npos) << endl; //if not found
-    for(char c1: compartment1){
-        int count = 0;
-        for(char c2: compartment2){
-
-            if(c1 == c2){
-                count++;
+    for (char c1: input1) {
+        for (char c2: input2) {
+            if (c1 == c2) {
+                if (find(result.begin(), result.end(), c1) == result.end()) {
+                    result.push_back(c1);
+                }
             }
-        }
-        if(count != 0){
-            resultMap.insert({c1, count});
         }
     }
 
-    return resultMap;
+    return result;
+}
+
+// jesus this is not optimal, this is truly not optimal.
+vector<char> findCommonFromThree(const string &rucksack1, const string &rucksack2, const string &rucksack3) {
+    vector<char> result;
+    for(char c1: rucksack1){
+        for (char c2: rucksack2){
+            for(char c3: rucksack3){
+                if(c1 == c2 && c2 == c3){
+                    if (find(result.begin(), result.end(), c1) == result.end()) {
+                        result.push_back(c1);
+                    }
+                }
+            }
+        }
+    }
+
+
+    return result;
 }
